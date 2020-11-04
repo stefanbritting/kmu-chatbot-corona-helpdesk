7cc80d5bf1b8eb877421a8b518f7cea2a09590b9
Stefan Britting <stefan.britting@t-online.de>
2020-11-04 17:59:58 +0100

    added craftbot api call capabilities

--- server.js
+++ server.js
@@ -20,7 +20,7 @@
 require('dotenv').config({silent: true});
 
 var server = require('./app');
-var port = process.env.PORT || 80;
+var port = process.env.PORT || 3000;
 
 server.listen(port, function() {
   // eslint-disable-next-line
